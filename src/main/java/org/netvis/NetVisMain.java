0f3ac1d30617e253dc30582d728c31d56afe93d0
Joe <joe@joe-Quattro-Power-Station-PRO-II>
2020-11-06 20:44:30 +0100

    add org.slf4j for logging

--- src/main/java/org/netvis/NetVisMain.java
+++ src/main/java/org/netvis/NetVisMain.java
@@ -1,9 +1,14 @@
 package org.netvis;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class NetVisMain {
 
+  private static final  Logger logger = LoggerFactory.getLogger(NetVisMain.class);
+  
   public static void main(String[] args) {
-    System.out.println("hello NetVisMain");
+    logger.info("info:NetVisMain.<ctor>() called.");
   }
 
 }
