044d7b10601035edfaa96d618251f7bd4fc9f57f
Joe <joe@joe-Quattro-Power-Station-PRO-II>
2020-11-07 12:46:23 +0100

    smaller formatting,remove pom.xml

--- src/main/java/org/netvis/NetVisPacketListener.java
+++ src/main/java/org/netvis/NetVisPacketListener.java
@@ -6,7 +6,7 @@ import org.pcap4j.packet.IpV4Packet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class NetVisPacketListener implements PacketListener{
+public class NetVisPacketListener implements PacketListener {
 
   private static final Logger logger = LoggerFactory.getLogger(NetVisPacketListener.class);
 
@@ -26,22 +26,19 @@ public class NetVisPacketListener implements PacketListener{
     counter++;
     timeOfLastPackage = System.currentTimeMillis();
     
-    if( mNVL.getPcapHandle() != null )
-    {
+    if( mNVL.getPcapHandle() != null ) {
        logger.trace("got Packet @{}", timeOfLastPackage );
     }
     
     IpV4Packet ipv4p = packet.get(IpV4Packet.class);
-    if( ipv4p != null )
-    {
+    if( ipv4p != null ) {
       logger.trace("received: [nr.: {}]----> IPV4: {} --> {}, len={}"
           , counter
           , ipv4p.getHeader().getSrcAddr()
           , ipv4p.getHeader().getDstAddr()
           , ipv4p.length());
       
-      synchronized( mNVL.getMain().getModel()  )
-      {
+      synchronized( mNVL.getMain().getModel() ){
         mNVL.getMain().getModel().addIPv4Packet( timeOfLastPackage, ipv4p  );
       }
     }
