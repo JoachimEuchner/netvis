8f4db8db56949a1713584b20dbdde7f9c12f5f96
Joe <joe@joe-Quattro-Power-Station-PRO-II>
2020-11-21 14:10:51 +0100

    check for more packet-types: ARP, ipv6. not processed yet

--- src/main/java/org/netvis/NetVisPacketListener.java
+++ src/main/java/org/netvis/NetVisPacketListener.java
@@ -2,7 +2,11 @@ package org.netvis;
 
 import org.pcap4j.core.PacketListener;
 import org.pcap4j.core.PcapPacket;
+import org.pcap4j.packet.ArpPacket;
+import org.pcap4j.packet.IcmpV4EchoPacket;
+import org.pcap4j.packet.IcmpV4TimeExceededPacket;
 import org.pcap4j.packet.IpV4Packet;
+import org.pcap4j.packet.IpV6Packet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,7 +31,7 @@ public class NetVisPacketListener implements PacketListener {
     timeOfLastPackage = System.currentTimeMillis();
     
     if( mNVL.getPcapHandle() != null ) {
-       logger.trace("got Packet @{}", timeOfLastPackage );
+       logger.trace("got Packet @{}ms", timeOfLastPackage );
     }
     
     IpV4Packet ipv4p = packet.get(IpV4Packet.class);
@@ -38,8 +42,38 @@ public class NetVisPacketListener implements PacketListener {
           , ipv4p.getHeader().getDstAddr()
           , ipv4p.length());
       
+      if (packet.contains(IcmpV4TimeExceededPacket.class)) 
+      {  
+        // IcmpV4TimeExceededPacket icmpTEp = packet.get(IcmpV4TimeExceededPacket.class);
+        logger.trace("received [nr.:{}] ---> got a IcmpV4TimeExceededPacket from {}", 
+            counter, ipv4p.getHeader().getSrcAddr());
+      }
+      
       synchronized( mNVL.getMain().getModel() ){
-        mNVL.getMain().getModel().addIPv4Packet( timeOfLastPackage, ipv4p  );
+        mNVL.getMain().getModel().addIPv4Packet( timeOfLastPackage, ipv4p  ); 
+      }
+    } else {
+      ArpPacket arpp = packet.get(ArpPacket.class);
+      if( arpp != null  ) {
+        logger.trace("received: [nr.: {}]----> ARP: {} --> {}, len={}"
+            , counter
+            , arpp.getHeader().getSrcProtocolAddr()
+            , arpp.getHeader().getDstProtocolAddr()
+            , arpp.length());
+        // TODO: handle ARP-Packet
+      } else  { 
+        IpV6Packet ipv6p = packet.get(IpV6Packet.class);
+        if( ipv6p != null ) {
+          logger.trace("received: [nr.: {}]----> IPV6: {} --> {}, len={}"
+              , counter
+              , ipv6p.getHeader().getSrcAddr()
+              , ipv6p.getHeader().getDstAddr()
+              , ipv6p.length());
+          // TODO: handle IPV6-Packet
+        }
+        else {
+          logger.trace("received: [nr.: {}] {}", counter, packet);
+        }
       }
     }
     
